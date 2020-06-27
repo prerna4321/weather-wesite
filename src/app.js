e5563d0242fd06e554d5b19122b54ca6af710d1d
Prerna <14102182prerna@gmail.com>
2020-06-28 01:15:29 +0530

    fixing port number

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ const forecast = require('./utils/forecast')
 
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 //define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public');
@@ -84,6 +85,6 @@ app.get('*', (req, res)=>{
     res.render('404', {title : "Error",name: 'Prerna Garg', body : "Errror 404 , page not found"});
 });
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000.')
+app.listen(port, () => {
+    console.log('Server is up on port ' +  3000)
 })
